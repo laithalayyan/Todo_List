448d11b02c2ff981b99d7e9eb0b3e3d58a05829b
laithalayyan <laith.nnn96@gmail.com>
2025-03-26 04:40:08 +0200

    Enhancements

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { TodoProvider } from './store/TodoStore';
+import { ThemeProvider } from './store/ThemeStore';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <TodoProvider>
+        <App />
+      </TodoProvider>
+    </ThemeProvider>
   </StrictMode>
 );
